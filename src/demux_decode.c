72cffa71817ea5469aad1fd43e2ba2d176a58b8a
iwander <2317791932@qq.com>
2023-04-17 18:03:22 +0800

    chore(base): add demux_decode

---
+++ src/demux_decode.c
@@ -0,0 +1,105 @@
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavformat/avio.h>
+#include <libavutil/file.h>
+
+#include "memory_stream.h"
+
+struct buffer_data {
+    uint8_t *ptr;
+    size_t size;
+};
+
+static int read_packet(void *opaque, uint8_t *buf, int buf_size)
+{
+    MemoryStream *const ms = (MemoryStream *const)opaque;
+    buf_size = FFMIN(buf_size, get_available_of_memory_stream(ms));
+
+    if (!buf_size)
+        return AVERROR_EOF;
+    printf("ptr:%p size:%zu\n", ms->data, ms->length);
+
+    /* copy internal buffer data to buf */
+    read_memory_stream(ms, buf, buf_size);
+    return buf_size;
+}
+
+int main(int argc, char *argv[])
+{
+    AVFormatContext *fmt_ctx = NULL;
+    AVIOContext *avio_ctx = NULL;
+    uint8_t *buffer = NULL, *avio_ctx_buffer = NULL;
+    size_t buffer_size, avio_ctx_buffer_size = 4096;
+    char *input_filename = NULL;
+    int ret = 0;
+    struct buffer_data bd = { 0 };
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s input_file\n"
+                "API example program to show how to read from a custom buffer "
+                "accessed through AVIOContext.\n", argv[0]);
+        return 1;
+    }
+    input_filename = argv[1];
+
+    /* slurp file content into buffer */
+    ret = av_file_map(input_filename, &buffer, &buffer_size, 0, NULL);
+    if (ret < 0)
+        goto end;
+
+    /* fill opaque structure used by the AVIOContext read callback */
+    bd.ptr  = buffer;
+    bd.size = buffer_size;
+
+    MemoryStream *const ms = create_memory_stream(bd.size);
+    write_memory_stream(ms, bd.ptr, bd.size);
+
+    if (!(fmt_ctx = avformat_alloc_context())) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+
+    avio_ctx_buffer = av_malloc(avio_ctx_buffer_size);
+    if (!avio_ctx_buffer) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+    avio_ctx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size,
+                                  0, ms, &read_packet, NULL, NULL);
+    if (!avio_ctx) {
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+    fmt_ctx->pb = avio_ctx;
+
+    ret = avformat_open_input(&fmt_ctx, NULL, NULL, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Could not open input\n");
+        goto end;
+    }
+
+    ret = avformat_find_stream_info(fmt_ctx, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Could not find stream information\n");
+        goto end;
+    }
+
+    av_dump_format(fmt_ctx, 0, input_filename, 0);
+
+end:
+    avformat_close_input(&fmt_ctx);
+
+    /* note: the internal buffer could have changed, and be != avio_ctx_buffer */
+    if (avio_ctx)
+        av_freep(&avio_ctx->buffer);
+    avio_context_free(&avio_ctx);
+
+    av_file_unmap(buffer, buffer_size);
+    free_memory_stream(ms);
+    if (ret < 0) {
+        fprintf(stderr, "Error occurred: %s\n", av_err2str(ret));
+        return 1;
+    }
+
+    return 0;
+}

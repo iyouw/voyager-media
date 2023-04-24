883f16b4353100fd83e02d2a2c50dff8efc0faea
iwander <2317791932@qq.com>
2023-04-24 14:53:38 +0800

    chore(base): add pthread to main

--- Makefile
+++ Makefile
@@ -1,47 +1,13 @@
-all:main
+src := src
 
-# VPATH variable
-# VPATH = src
+.PHONY: main demux_decode
 
-# vpath directive
-vpath %.c src
-vpath %.h src
+main:
+	$(MAKE) main --directory=$(src)
 
-# which compiler
-CC = cc
 
-# where are include files kept
-INCLUDE = .:/usr/local/include
-
-# options for dev
-CFLAGS = -g -Wall
-
-# options for release
-# CFLAGS = -O -Wall -ansi
-
-FFMPEG_LIBS=    libavdevice                        \
-                libavformat                        \
-                libavfilter                        \
-                libavcodec                         \
-                libswresample                      \
-                libswscale                         \
-                libavutil                          \
-
-FLAGS += -Wall -g
-FLAGS := $(shell pkg-config --cflags $(FFMPEG_LIBS)) $(FLAGS)
-LDLIBS := $(shell pkg-config --libs $(FFMPEG_LIBS)) $(LDLIBS)
-
-main: main.o memory_stream.o
-	$(CC) -o main main.o memory_stream.o -lm -lpthread
-
-main.o: main.c
-	$(CC) -I$(INCLUDE) $(CFLAGS) -c $<
-
-memory_stream.o: memory_stream.c  memory_stream.h
-	$(CC) -I$(INCLUDE) $(CFLAGS) -c $<
-
-demux_decode: demux_decode.c memory_stream.c memory_stream.h
-	$(CC) -I$(INCLUDE) $(CFLAGS) -o demux_decode demux_decode.c memory_stream.c -L../lib -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -pthread -lm -latomic
+demux_decode:
+	${MAKE} demux_decode --directory=$(src)
 
 clean:
-	-rm -f main demux_decode *.o *.wasm
+	${MAKE} clean --directory=$(src)
--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include "memory_stream.h"
+
+#define READ_FILE_BUFFER_SIZE (7 * 1024)
+#define WRITE_FILE_BUFFER_SIZE (3 * 1024)
 
 typedef enum { TRUE, FALSE } Boolean;
 
@@ -9,10 +13,73 @@ typedef struct
 {
   const char *fileName;
   struct MemoryStream *const ms;
-} MemoryContext;
+} Context;
 
 Boolean isDone = FALSE;
 pthread_mutex_t mutex;
+
+static void *read_file_thread(void *args)
+{
+  int ret = 0;
+  Context *ctx = (Context *)args;
+  FILE *file = fopen(ctx->fileName, "rb");
+  if (file == NULL) {
+    ret = 100;
+    goto fail;
+  }
+
+  uint8_t buffer[READ_FILE_BUFFER_SIZE];
+  size_t bytes_read;
+
+  while(!feof(file)) {   
+    bytes_read = fread(buffer, sizeof(uint8_t), READ_FILE_BUFFER_SIZE, file);
+    ret = pthread_mutex_lock(&mutex);
+    if (ret != 0) goto fail;
+    write_memory_stream(ctx->ms, buffer, bytes_read);
+    ret = pthread_mutex_unlock(&mutex);
+    if (ret != 0) goto fail;
+    sleep(1);
+  }
+
+  pthread_mutex_lock(&mutex);
+  isDone = TRUE;
+  pthread_mutex_unlock(&mutex);
+fail:
+  pthread_exit(&ret);
+}
+
+
+static void *write_file_thread(void *args)
+{
+  int ret = 0;
+  Context *ctx = (Context *)args;
+  FILE *file = fopen(ctx->fileName, "wb");
+  if (file == NULL) {
+    ret = 101;
+    goto fail;
+  }
+  uint8_t buffer[WRITE_FILE_BUFFER_SIZE];
+  size_t bytes_read, bytes_write;
+  Boolean done;
+  while(1) {
+    ret = pthread_mutex_lock(&mutex);
+    if (ret != 0) goto fail;
+    bytes_read = read_memory_stream(ctx->ms, buffer, WRITE_FILE_BUFFER_SIZE);
+    done = isDone;
+    ret = pthread_mutex_unlock(&mutex);
+    if (ret != 0) goto fail;
+    bytes_write = fwrite(buffer, sizeof(uint8_t), bytes_read, file);
+    if (bytes_write != bytes_read) {
+      ret = 102;
+      break;
+    }
+    if (bytes_read < WRITE_FILE_BUFFER_SIZE && done == TRUE) goto fail;
+    sleep(1);
+  }
+fail:
+  pthread_exit(&ret);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -25,56 +92,49 @@ int main(int argc, char *argv[])
   int ret = 0;
   const char *in = argv[1];
   const char *out = argv[2];
+  MemoryStream *ms = create_memory_stream(1024);
 
-  MemoryStream *ms = create_memory_stream(100);
-
-  FILE *is = fopen(in, "rb");
-  FILE *os = fopen(out, "wb");
-  if (is == NULL)
-  {
-    fprintf(stderr, "can not open file: %s", in);
-    ret = -1;
+  ret = pthread_mutex_init(&mutex, NULL);
+  if (ret != 0) {
+    perror("init mutex failed!");
     goto fail;
   }
 
-  if (os == NULL)
-  {
-    fprintf(stderr, "can not open or create file: %s", out);
-    ret = -2;
+  Context read_context = {
+    .fileName = in,
+    ms = ms
+  };
+  pthread_t read_thread;
+  ret = pthread_create(&read_thread, NULL, read_file_thread, &read_context);
+  if (ret != 0){
+    perror("create read thread failed");
     goto fail;
   }
 
-  fseek(is, 0, SEEK_END);
-  const size_t size = ftell(is);
-
-  uint8_t read_buffer[100];
-
-  uint8_t buffer[200];
-
-  const size_t sz = size - 200;
-  uint8_t *buffer2 = malloc(sz);
-
-  fseek(is, 0, SEEK_SET);
-  fread(buffer, sizeof(uint8_t), sizeof(buffer), is);
-  write_memory_stream(ms, buffer, 200);
-
-  size_t rds = 0;
-  while ((rds = read_memory_stream(ms, read_buffer, 100)) > 0)
-  {
-    fwrite(read_buffer, sizeof(uint8_t), rds, os);
+  Context write_context = {
+    .fileName = out,
+    .ms = ms
+  };
+  pthread_t write_thread;
+  ret = pthread_create(&write_thread, NULL, write_file_thread, &write_context);
+  if (ret != 0) {
+    perror("create write thread failed");
+    goto fail;
   }
 
-  fread(buffer2, sizeof(uint8_t), sz, is);
-  write_memory_stream(ms, buffer2, sz);
+  ret = pthread_join(read_thread, NULL);
+  if (ret != 0) {
+    perror("read thread join failed");
+    goto fail;
+  }
 
-  while ((rds = read_memory_stream(ms, read_buffer, 100)) > 0)
-  {
-    fwrite(read_buffer, sizeof(uint8_t), rds, os);
+  ret = pthread_join(write_thread, NULL);
+  if (ret != 0){
+    perror("write thread join failed");
+    goto fail;
   }
 fail:
-  if (is) fclose(is);
-  if (os) fclose(os);
-  if (buffer2) free(buffer2);
-  if (ms) free_memory_stream(ms);
+  free_memory_stream(ms);
+  pthread_mutex_destroy(&mutex);
   return ret;
 }
